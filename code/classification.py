10ac6b172e8981fef9f874cf375ae95e2c20f0ed
David Feng <david.feng@alleninstitute.org>
2024-12-20 17:42:59 +0000

    adding border classification

--- code/classification.py
+++ code/classification.py
@@ -13,7 +13,8 @@ import roicat
 import sparse
 from pint import UnitRegistry
 from typing import List
-from plots import plot_probabilities, plot_predictions
+from plots import plot_probabilities, plot_predictions, plot_border_rois
+
 
 @dataclass
 class Plane:
@@ -24,8 +25,9 @@ class Plane:
     rois: np.array
 
 
-
-def classify_plane(rois: np.array, um_per_pixel: float, soma_classifier, dendrite_classifier):
+def classify_plane(
+    rois: np.array, um_per_pixel: float, soma_classifier, dendrite_classifier
+):
     # ROInet embedding
     roinet = roicat.ROInet.ROInet_embedder(
         device=roicat.helpers.set_device(),  ## Which torch device to use ('cpu', 'cuda', etc.)
@@ -40,7 +42,7 @@ def classify_plane(rois: np.array, um_per_pixel: float, soma_classifier, dendrit
     data = roicat.data_importing.Data_roicat()
     data.set_spatialFootprints(rois, um_per_pixel=um_per_pixel)
     data.transform_spatialFootprints_to_ROIImages(out_height_width=(36, 36))
-    
+
     roinet.generate_dataloader(
         ROI_images=data.ROI_images,  ## Input images of ROIs
         um_per_pixel=data.um_per_pixel,  ## Resolution of FOV
@@ -51,7 +53,13 @@ def classify_plane(rois: np.array, um_per_pixel: float, soma_classifier, dendrit
     soma_predictions, soma_probabilities = soma_classifier(roinet.latents)
     dendrite_predictions, dendrite_probabilities = dendrite_classifier(roinet.latents)
 
-    return soma_predictions, soma_probabilities, dendrite_predictions, dendrite_probabilities
+    return (
+        soma_predictions,
+        soma_probabilities,
+        dendrite_predictions,
+        dendrite_probabilities,
+    )
+
 
 def find_um_per_pixel(input_dir: Path) -> float:
     # get um_per_pixel and dims (FOV size) from session.json
@@ -74,26 +82,45 @@ def find_um_per_pixel(input_dir: Path) -> float:
 
     return um_per_pixel
 
+
 def prepare_plane(extraction_file: Path, output_dir: Path) -> List[Plane]:
     plane_name = path.parts[-3]
     plane_dir = output_dir / "classification" / plane_name
     plane = Plane(
-        name=plane_name, 
+        name=plane_name,
         input_extraction_file=path,
         output_dir=plane_dir,
         output_classification_file=plane_dir / "classification.h5",
-        rois=load_extraction_rois(extraction_file)
+        rois=load_extraction_rois(extraction_file),
     )
-    
+
     plane.output_dir.mkdir(parents=True, exist_ok=True)
-    
+
     return plane
+
 
 def load_extraction_rois(extraction_file: Path) -> np.array:
     with h5py.File(extraction_file) as f:
-        return sparse.COO(
-            f["rois/coords"], f["rois/data"], f["rois/shape"]
-        ).todense()
+        return sparse.COO(f["rois/coords"], f["rois/data"], f["rois/shape"]).todense()
+
+
+def classify_border_rois(rois: np.array, border_size: int) -> np.array:
+    masks = np.where(rois > 0)
+    border_rois = set()
+    border_rois.update(masks[0][np.where(masks[1] < border_size)[0]].tolist())
+    border_rois.update(masks[0][np.where(masks[2] < border_size)[0]].tolist())
+    border_rois.update(
+        masks[0][np.where(masks[1] >= rois.shape[1] - border_size)[0]].tolist()
+    )
+    border_rois.update(
+        masks[0][np.where(masks[2] >= rois.shape[2] - border_size)[0]].tolist()
+    )
+
+    border_rois_bool = np.zeros(shape=rois.shape[0], dtype=bool)
+    border_rois_bool[list(border_rois)] = True
+
+    return border_rois_bool
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -121,13 +148,19 @@ if __name__ == "__main__":
         default="/data/2p_roi_classifier/dendrite.classification_training.autoclassifier.onnx",
         help="Path of the classifier model, comma-delimited",
     )
+    parser.add_argument(
+        "--border-size",
+        type=int,
+        default=10,
+        help="Distance from border for ROI to be classified on the border",
+    )
     start_time = dt.now(tz.utc)
     args = parser.parse_args()
     input_dir = Path(args.input_dir).resolve()
     output_dir = Path(args.output_dir).resolve()
 
     um_per_pixel = find_um_per_pixel(input_dir)
-    
+
     soma_classifier = (
         roicat.classification.classifier.Load_ONNX_model_sklearnLogisticRegression(
             args.soma_classifier_path
@@ -140,20 +173,31 @@ if __name__ == "__main__":
         )
     )
 
-    for path in Path(input_dir).rglob('*extraction.h5'):
+    for path in Path(input_dir).rglob("*extraction.h5"):
         plane = prepare_plane(path, output_dir)
 
-        soma_predictions, soma_probabilities, dendrite_predictions, dendrite_probabilities = classify_plane(
-            rois=plane.rois, 
+        (
+            soma_predictions,
+            soma_probabilities,
+            dendrite_predictions,
+            dendrite_probabilities,
+        ) = classify_plane(
+            rois=plane.rois,
             um_per_pixel=um_per_pixel,
             soma_classifier=soma_classifier,
-            dendrite_classifier=dendrite_classifier,             
+            dendrite_classifier=dendrite_classifier,
         )
-        
+
+        border_rois = classify_border_rois(
+            rois=plane.rois, border_size=args.border_size
+        )
+
         ax = plot_probabilities(plane.rois, soma_probabilities, "soma probabilities")
         plt.savefig(str(plane.output_dir / "soma_probabilities.png"))
 
-        ax = plot_probabilities(plane.rois, dendrite_probabilities, "dendrite probabilities")
+        ax = plot_probabilities(
+            plane.rois, dendrite_probabilities, "dendrite probabilities"
+        )
         plt.savefig(str(plane.output_dir / "dendrite_probabilities.png"))
 
         ax = plot_predictions(plane.rois, soma_predictions, "soma predictions")
@@ -161,7 +205,10 @@ if __name__ == "__main__":
 
         ax = plot_predictions(plane.rois, dendrite_predictions, "dendrite predictions")
         plt.savefig(str(plane.output_dir / "dendrite_predictions.png"))
-        
+
+        ax = plot_border_rois(plane.rois, border_rois, "border ROIs")
+        plt.savefig(str(plane.output_dir / "border_rois.png"))
+
         # save results
         with h5py.File(plane.output_classification_file, "w") as f:
             g = f.create_group("soma")
@@ -171,3 +218,6 @@ if __name__ == "__main__":
             g = f.create_group("dendrites")
             g.create_dataset("predictions", data=dendrite_predictions)
             g.create_dataset("probabilities", data=dendrite_probabilities)
+
+            g = f.create_group("border")
+            g.create_dataset("labels", data=border_rois)
--- code/plots.py
+++ code/plots.py
@@ -3,22 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 TRANSPARENT_TO_RED = LinearSegmentedColormap.from_list(
-    "TransparentToRed", 
-    [(1, 0, 0, 0), (1, 0, 0, 1)]
+    "TransparentToRed", [(1, 0, 0, 0), (1, 0, 0, 1)]
 )
 
+
 def plot_probabilities(rois, probabilities, title):
-    prob_rois = (rois>0) * probabilities[:, 1, np.newaxis, np.newaxis]
+    prob_rois = (rois > 0) * probabilities[:, 1, np.newaxis, np.newaxis]
     fig, ax = plt.subplots()
-    ax.imshow(rois.sum(axis=0), cmap='gray')
-    im = ax.imshow(prob_rois.sum(axis=0), cmap=TRANSPARENT_TO_RED, clim=[0,1], alpha=.9)
+    ax.imshow(rois.sum(axis=0), cmap="gray")
+    im = ax.imshow(
+        prob_rois.sum(axis=0), cmap=TRANSPARENT_TO_RED, clim=[0, 1], alpha=0.9
+    )
     ax.set_title(title)
     return ax
 
+
 def plot_predictions(rois, predictions, title):
-    pred_rois = (rois>0) * predictions[:, np.newaxis, np.newaxis]
+    pred_rois = (rois > 0) * predictions[:, np.newaxis, np.newaxis]
     fig, ax = plt.subplots()
-    ax.imshow(rois.sum(axis=0), cmap='gray')
-    ax.imshow(pred_rois.sum(axis=0), cmap=TRANSPARENT_TO_RED, alpha=.9)
+    ax.imshow(rois.sum(axis=0), cmap="gray")
+    ax.imshow(pred_rois.sum(axis=0), cmap=TRANSPARENT_TO_RED, alpha=0.9)
+    ax.set_title(title)
+    return ax
+
+
+def plot_border_rois(rois, border_rois, title):
+    border_rois_mask = (rois > 0) * border_rois[:, np.newaxis, np.newaxis]
+    fig, ax = plt.subplots()
+    ax.imshow(rois.sum(axis=0), cmap="gray")
+    ax.imshow(border_rois_mask.sum(axis=0), cmap=TRANSPARENT_TO_RED, alpha=0.9)
     ax.set_title(title)
     return ax
